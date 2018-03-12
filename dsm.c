5463b2ed4519833bed81792cb363f03470c66c6b
eudaemonal <eudaemonal@gmail.com>
2018-03-12 15:06:38 +1100

    update

--- dsm.c
+++ dsm.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include "sm.h"
-
 
 
 void sm_fork(int n){
 	/* ssh to server */
-
+	printf("sm_fork: %d\n", n);
 }
 
 
@@ -13,24 +13,25 @@ int main(int argc, char **argv){
 	int i = 0;
 	while(i<argc){
 		printf("arg %d : %s\n", i, argv[i]);
-		/*
-		switch(argv){
-			case "-H":
 
-			case "-h":
-
-			case "-l":
-
-			case "-n":
-					n = argv[++i];
-					sm_fork(n);
-					break
-			case "-v": printf("version information\n");
-					break;
+		if(strcmp(argv[i], "-H")==0){
+			printf("list of host names\n");
 		}
-		*/
+		else if(strcmp(argv[i], "-h")==0){
+			printf("this usage message\n");
+		}
+		else if(strcmp(argv[i], "-l")==0){
+			printf("log each significant allocator action to LOGFILE \n");
+		}
+		else if(strcmp(argv[i], "-n")==0){
+			sm_fork(atoi(argv[++i]));		
+		}
+		else if(strcmp(argv[i], "-v")==0){
+			printf("version information\n");
+		}
 		
 		i++;
-	}
 
-}
+	}/* end while */
+
+}/* end main */
