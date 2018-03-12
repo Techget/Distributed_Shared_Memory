0798e93967ad37830121741b8416d6ec95552111
eudaemonal <eudaemonal@gmail.com>
2018-03-12 21:50:53 +1100

    update socket

--- sm_socket.c
+++ sm_socket.c
@@ -11,9 +11,57 @@ No fixed port numbers
 pass information (hostname, portnumber) as command line argumant
 */
 
-#define PORT 8080
+/*
+	client create socket
+*/
+int sm_create_socket(char *hostname, int port){
+    int sock = 0;
+    struct sockaddr_in serv_addr;
 
-int sm_create_socket(){
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+      
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, hostname, &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+}
+
+
+
+int sm_socket_send(int sock, char *buffer){
+    send(sock , buffer , strlen(buffer) , 0 );
+    printf("message sent\n");
+}
+
+
+
+int sm_socket_receive(int sock, char *buffer){
+	int valread;
+    valread = read(sock , buffer, 1024);
+    printf("%s\n",buffer );
+
+}
+/*
+	Allocator connect to peer
+*/
+int sm_connect_to_peer(int port){
 	int server_fd;
 	int opt = 1;
 	struct sockaddr_in address;
@@ -31,7 +79,7 @@ int sm_create_socket(){
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(port);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
@@ -42,12 +90,7 @@ int sm_create_socket(){
 	return 0;
 }
 
-
-int sm_connect_to_peer(){
-	return 0;
-}
-
-int sm_send(int server_fd, struct sockaddr_in address, char *buffer){
+int sm_send_to_peer(int server_fd, struct sockaddr_in address, char *buffer){
 	int new_socket;
 	int addrlen = sizeof(address);
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
@@ -60,7 +103,7 @@ int sm_send(int server_fd, struct sockaddr_in address, char *buffer){
 }
 
 
-int sm_receive(int server_fd, struct sockaddr_in address, char *buffer){
+int sm_receive_from_peer(int server_fd, struct sockaddr_in address, char *buffer){
 	int new_socket, valread;
 	int addrlen = sizeof(address);
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
