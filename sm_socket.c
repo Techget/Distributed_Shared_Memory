16e0421f7b694099811465660666220429bb011e
eudaemonal <eudaemonal@gmail.com>
2018-03-13 16:34:13 +1100

    sm_socket done

--- sm_socket.c
+++ sm_socket.c
@@ -41,38 +41,24 @@ int sm_create_socket(char *hostname, int port){
         printf("\nConnection Failed \n");
         return -1;
     }
+	return sock;
 }
 
-
-
-int sm_socket_send(int sock, char *buffer){
-    send(sock , buffer , strlen(buffer) , 0 );
-    printf("message sent\n");
-}
-
-
-
-int sm_socket_receive(int sock, char *buffer){
-	int valread;
-    valread = read(sock , buffer, 1024);
-    printf("%s\n",buffer );
-
-}
 /*
 	Allocator connect to peer
 */
 int sm_connect_to_peer(int port){
-	int server_fd;
+	int sock, new_socket;
 	int opt = 1;
 	struct sockaddr_in address;
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	int addrlen = sizeof(address);
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -81,36 +67,36 @@ int sm_connect_to_peer(int port){
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    if (bind(sock, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-
-	return 0;
-}
-
-int sm_send_to_peer(int server_fd, struct sockaddr_in address, char *buffer){
-	int new_socket;
-	int addrlen = sizeof(address);
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+    if (listen(sock, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(sock, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
     {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-	send(new_socket , buffer , strlen(buffer) , 0 );
-	return 0;
+
+	return new_socket;
 }
 
 
-int sm_receive_from_peer(int server_fd, struct sockaddr_in address, char *buffer){
-	int new_socket, valread;
-	int addrlen = sizeof(address);
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-	valread = read(new_socket , buffer, 1024);
-	return 0;
+
+int sm_send(int sock, char *buffer){
+    send(sock , buffer , strlen(buffer) , 0 );
+
 }
+
+
+
+int sm_receive(int sock, char *buffer){
+	int valread;
+    valread = read(sock , buffer, 1024);
+}
+
--- sm_socket.h
+++ sm_socket.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 
 
+int sm_create_socket(char *hostname, int port);
 
+int sm_connect_to_peer(int port);
+
+int sm_send(int sock, char *buffer);
+
+int sm_receive(int sock, char *buffer);
 
 #endif /* !_SM_SOCKET_H  */
