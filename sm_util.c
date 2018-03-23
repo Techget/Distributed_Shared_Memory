3b16d46e6c95199598a29e76e6b5528623976811
eudaemonal <eudaemonal@gmail.com>
2018-03-23 20:41:20 +1100

    update

--- sm_util.c
+++ sm_util.c
@@ -2,14 +2,15 @@
 
 
 
-
+/* Extract oct number from string 
+*/
 long get_number(char *str){
 	char *p = str;
 	long val;
-	while (*p) { // While there are more characters to process...
-	    if (isdigit(*p)) { // Upon finding a digit, ...
-	        val = strtol(p, &p, 10); // Read a number, ...
-	    } else { // Otherwise, move on to the next character.
+	while (*p) { 
+	    if (isdigit(*p)) { 
+	        val = strtol(p, &p, 10); 
+	    } else { 
 	        p++;
 	    }
 	}
