f57674285fa7ab2973a92a9d233cfa1b65a6a465
eudaemonal <eudaemonal@gmail.com>
2018-03-31 15:21:49 +1100

    fix log2 problem

--- sm_util.c
+++ sm_util.c
@@ -1,5 +1,27 @@
 #include "sm_util.h"
 
+#include <stdint.h>
+
+
+int log2_64(uint64_t n)
+{
+    static const int table[64] = {
+        0, 58, 1, 59, 47, 53, 2, 60, 39, 48, 27, 54, 33, 42, 3, 61,
+        51, 37, 40, 49, 18, 28, 20, 55, 30, 34, 11, 43, 14, 22, 4, 62,
+        57, 46, 52, 38, 26, 32, 41, 50, 36, 17, 19, 29, 10, 13, 21, 56,
+        45, 25, 31, 35, 16, 9, 12, 44, 24, 15, 8, 23, 7, 6, 5, 63 };
+
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n |= n >> 32;
+
+    return table[(n * 0x03f6eaf2cd271461) >> 58];
+}
+
+
 void printHelpMsg() {
 	printf(" Usage: dsm [OPTION]... EXECUTABLE-FILE NODE-OPTION...\n");
 }
@@ -12,7 +34,7 @@ int recorderFindNidSetToOne(unsigned long * recorder) {
 	// int temp = (*recorder) ^ ((*recorder) & ((*recorder)-1));
 	// temp -= 1; // to restore to nid;
 	// return temp;
-    int temp = log2(*(recorder) & (-(*recorder)));
+    int temp = log2_64(*(recorder) & (-(*recorder)));
 	return temp;
 }
 
@@ -85,6 +107,10 @@ void * getPageBaseOfAddr(void * addr) {
 	void * page_base = (void *)((int)addr & ~(getpagesize()-1));
 	return page_base;
 }
+
+
+
+
 
 /* Extract oct number from string 
 	decode message with digit and '-' only, unable to decode hex
