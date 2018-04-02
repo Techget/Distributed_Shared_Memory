cc784621ed5a28708dd465077947094d8ab35823
Techget Xuan <txmanialife@gmail.com>
2018-04-02 14:22:16 +1000

    add queue data structure

---
+++ queue.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Queue
+{
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+};
+
+struct Queue* createQueue(unsigned capacity);
+int isFull(struct Queue* queue); 
+int isEmpty(struct Queue* queue);
+void enqueue(struct Queue* queue, int item);
+int dequeue(struct Queue* queue);
+int front(struct Queue* queue);
+int rear(struct Queue* queue);
